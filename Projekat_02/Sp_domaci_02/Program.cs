5f54ef23beb7b81d0128b840d1dafcc6957efb5b
KingLaza <lazarbecic1531@gmail.com>
2024-05-28 23:07:42 +0200

    Project_02: trivial changes from the copyed Project_01, like the name and notes

---
+++ Projekat_02/Sp_domaci_02/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Projekat_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prefix = "http://localhost:5050/";
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(prefix);
+            listener.Start();
+            Console.WriteLine("Listening for requests on " + prefix);
+
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(ProcessRequest, listener.GetContext());
+            }
+        }
+
+        static void ProcessRequest(object state)
+        {
+            Stopwatch stopwatch= Stopwatch.StartNew();
+            HttpListenerContext context = (HttpListenerContext)state;
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string filename = "NOTHING";
+
+            if (request.Url.Segments.Length <= 1)               //just tinker this a bit, maybe there are other annomalies
+            {
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                string responseString = "Filename is missing in the URL.";
+                byte[] buffer2 = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer2.Length;
+                response.OutputStream.Write(buffer2, 0, buffer2.Length);
+                response.OutputStream.Close();
+                return;
+            }
+                
+
+            if (request.Url.Segments.Length > 1)
+                filename = request.Url.Segments[1];
+            string responseBody;
+            responseBody = Cache.ReadFromCache(filename);
+            if (responseBody == "")
+            {
+                string current_folder = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+                current_folder = Directory.GetParent(current_folder).FullName;
+                current_folder = Directory.GetParent(current_folder).FullName;
+                string filePath = FindFile(current_folder, filename);
+
+                if (filePath == null)
+                {
+                    responseBody = $"File '{filename}' not found.";
+                }
+                else
+                {
+                    string fileContent = File.ReadAllText(filePath);
+                    int wordCount = CountWordsStartingWithCapital(fileContent);
+                    responseBody = $"Word count for file '{filename}': {wordCount}";
+                }
+                Cache.WriteToCache(filename, responseBody);
+            }
+
+            stopwatch.Stop();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(responseBody);
+            double vreme = stopwatch.Elapsed.TotalMilliseconds;
+            sb.Append("/Proteklo vreme: "+vreme.ToString()+"ms");
+
+            //byte[] buffer = Encoding.UTF8.GetBytes(responseBody);
+            byte[] buffer = Encoding.UTF8.GetBytes(sb.ToString());
+            response.ContentType = "text/plain";
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.Close();
+        }
+
+        static string FindFile(string directory, string filename)       //ovo nzm da li je potrebno da rade vise niti...
+        {
+            string[] files = Directory.GetFiles(directory, filename, SearchOption.AllDirectories);
+            if (files.Length > 0)
+            {
+                return files[0];
+            }
+            return null;
+        }
+
+        static int CountWordsStartingWithCapital(string text)           //ovo bi mozda moglo da se optimizuje da ga rade vise niti
+        {
+            int count = 0;
+            string[] words = text.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (char.IsUpper(word[0]) && word.Length > 5)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
